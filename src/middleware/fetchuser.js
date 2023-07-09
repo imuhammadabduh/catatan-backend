7d2e27faa75aabbda962c6a1ca8a4a5687c59177
imuhammadabduh <imuhammadabduh@gmail.com>
2023-07-09 21:04:53 +0700

    merbarui vercel json

--- src/middleware/fetchuser.js
+++ src/middleware/fetchuser.js
@@ -4,14 +4,14 @@ const JWT_SECRET = "Pyaardimenukichahida";
 const fetchuser = (req, res, next) => {
   const token = req.header("auth-token");
   if (!token) {
-    res.status(401).send({ error: "Please authenticate using a valid token" });
+    res.status(401).send({ error: "token tidak valid" });
   }
   try {
     const data = jwt.verify(token, JWT_SECRET);
     req.user = data.user;
     next();
   } catch (error) {
-    res.status(401).send({ error: "TdfRY M df" });
+    res.status(401).send({ error: "token tidak valid" });
   }
 };
 
