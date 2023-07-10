b16c759733bbf08f7760300c47c7285f2e896127
imuhammadabduh <imuhammadabduh@gmail.com>
2023-07-10 11:52:10 +0700

    error resnpne note

--- src/middleware/middlewareValidation.js
+++ src/middleware/middlewareValidation.js
@@ -1,10 +1,13 @@
 const { validationResult } = require("express-validator");
+const dataResponse = require("../utils/dataResponse");
 
 function middlewareValidation(req, res, next) {
   const errors = validationResult(req);
-  console.log(errors)
+  console.log(errors);
   if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
+    return res
+      .status(400)
+      .json(dataResponse(false, "error validation", errors));
   }
   return next();
 }
